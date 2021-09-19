595804a5f86d61b02e8b797f4dff387b84723f46
Pavel <45289168+paruz@users.noreply.github.com>
2021-09-19 10:51:39 +0300

    Add files via upload

---
+++ rabota5/widget.cpp
@@ -0,0 +1,16 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+//6 расчитать площадь треугольник
+//7 расчитать площадь многоугольника
